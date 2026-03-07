45ddbc413e233e9d7c5a813f4a317e72ab41be4b
haritramani10 <haritramani10@gmail.com>
2026-03-07 12:59:59 +0530

    Update about.php

--- about.php
+++ about.php
@@ -4,6 +4,7 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -127,3 +128,4 @@ session_start();
 </body>
 
 </html>
+
